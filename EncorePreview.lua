32f6505e095694f37b0136751096794fb4281655
NarrikSynthfox <80410683+NarrikSynthfox@users.noreply.github.com>
2024-02-04 03:26:40 -0500

    basic beat lines drawing

--- EncorePreview.lua
+++ EncorePreview.lua
@@ -1,112 +1,72 @@
-local function rgb2num(red, green, blue)
-	green = green * 256
-	blue = blue * 256 * 256
-	return red + green + blue
-end
-
-local r, g, b = 64,64,64
-gfx.clear = rgb2num(r, g, b)
-
-gfx.init("Encore Preview", 640, 480, 0, 200, 200)
-
+version_num="0.0.5"
+imgScale=480/1024
 inst=1
 diff=4
-midihash=""
+midiHash=""
+beatHash=""
 trackSpeed=2
 pR={
 	{{60,63},{66,69}},
 	{{72,75},{78,81}},
 	{{84,87},{90,93}},
 	{{96,100},{102,106}}
-}
-oP=116
+} --pitch ranges {{notes},{lift markers}} for each difficulty
+oP=116 --overdrive pitch
 offset=0
 diffNames={"Easy","Medium","Hard","Expert"}
+
+--note rendering vars
 notes={}
-imgscale=math.min(gfx.w,gfx.h)/1024
-
-local script_folder = string.gsub(debug.getinfo(1).source:match("@?(.*[\\|/])"),"\\","/")
-hwy_emh = gfx.loadimg(0,script_folder.."assets/hwy_emh.png")
-hwy_x = gfx.loadimg(1,script_folder.."assets/hwy_x.png")
-note = gfx.loadimg(2,script_folder.."assets/note.png")
-note_o = gfx.loadimg(3,script_folder.."assets/note_o.png")
-lift = gfx.loadimg(4,script_folder.."assets/lift.png")
-lift_o = gfx.loadimg(5,script_folder.."assets/lift_o.png")
-lift_invalid = gfx.loadimg(6,script_folder.."assets/lift_invalid.png")
-
+beatLines={}
 curBeat=0
+curBeatLine=1
 curNote=1
+nxoff=152 --x offset
+nxm=0.05 --x mult of offset
+nyoff=192 --y offset
+nsm=0.05 --scale multiplier
 
-nxs=152
-nxm=0.05
-nyoff=192
-nsm=0.05
+lastCursorTime=reaper.TimeMap2_timeToQN(reaper.EnumProjects(-1),reaper.GetCursorPosition())
+
+
+local function rgb2num(r, g, b)
+	g = g * 256
+	b = b * 256 * 256
+	return r + g + b
+end
+
 function getNoteIndex(time, lane)
-    for i, note in ipairs(notes) do
-        if note[1] == time and note[3] == lane then
-            return i
-        end
-    end
-    return -1
+	for i, note in ipairs(notes) do
+		if note[1] == time and note[3] == lane then
+			return i
+		end
+	end
+	return -1
 end
 
 function findTrack(trackName)
-    local numTracks = reaper.CountTracks(0)
-    for i = 0, numTracks - 1 do
-        local track = reaper.GetTrack(0, i)
-        local _, currentTrackName = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
-        if currentTrackName == trackName then
-            return track
-        end
-    end
-    return nil
-end
-
-function parseNotes(take)
-    notes = {}
-	od_phrases={}
-	od=false
-	cur_od_phrase=1
-    _, notecount = reaper.MIDI_CountEvts(take)
-
-    for i = 0, notecount - 1 do
-        _, _, _, spos, epos, _, pitch, _ = reaper.MIDI_GetNote(take, i)
-        ntime = reaper.MIDI_GetProjQNFromPPQPos(take, spos)
-        nend = reaper.MIDI_GetProjQNFromPPQPos(take, epos)
-
-        if pitch == oP then
-			table.insert(od_phrases, {ntime,nend})
-        elseif pitch >= pR[diff][1][1] and pitch <= pR[diff][1][2] then
-            lane = pitch - pR[diff][1][1]
-			noteIndex = getNoteIndex(ntime, lane)
-			if noteIndex ~= -1 then
-                notes[noteIndex][2] = nend-ntime
-            else
-				table.insert(notes, { ntime, nend - ntime, lane, false, false })
-            end
-        elseif pitch >= pR[diff][2][1] and pitch <= pR[diff][2][2] then
-            lane = pitch - pR[diff][2][1]
-            noteIndex = getNoteIndex(ntime, lane)
-
-            if noteIndex ~= -1 then
-                notes[noteIndex][4] = true
-            else
-                table.insert(notes, { ntime, -1, lane, true, false })
-            end
-        end
-    end
-	if #od_phrases~=0 then
-		for i=1,#notes do
-			if notes[i][1]>od_phrases[cur_od_phrase][2] then
-				if cur_od_phrase<#od_phrases then cur_od_phrase=cur_od_phrase+1 end
-			end
-			if notes[i][1]>=od_phrases[cur_od_phrase][1] and notes[i][1]<=od_phrases[cur_od_phrase][2] then
-				notes[i][5]=true
-			end
+	local numTracks = reaper.CountTracks(0)
+	for i = 0, numTracks - 1 do
+		local track = reaper.GetTrack(0, i)
+		local _, currentTrackName = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
+		if currentTrackName == trackName then
+			return track
 		end
 	end
+	return nil
 end
 
+gfx.clear = rgb2num(64, 64, 64)
+gfx.init("Encore Preview", 640, 480, 0, 200, 200)
+
+	local script_folder = string.gsub(debug.getinfo(1).source:match("@?(.*[\\|/])"),"\\","/")
+	hwy_emh = gfx.loadimg(0,script_folder.."assets/hwy_emh.png")
+	hwy_x = gfx.loadimg(1,script_folder.."assets/hwy_x.png")
+	note = gfx.loadimg(2,script_folder.."assets/note.png")
+	note_o = gfx.loadimg(3,script_folder.."assets/note_o.png")
+	lift = gfx.loadimg(4,script_folder.."assets/lift.png")
+	lift_o = gfx.loadimg(5,script_folder.."assets/lift_o.png")
+	lift_invalid = gfx.loadimg(6,script_folder.."assets/lift_invalid.png")
 
 instrumentTracks={
 	{"Drums",findTrack("PART DRUMS")},
@@ -119,6 +79,51 @@ eventTracks={
 	findTrack("EVENTS"),
 	findTrack("BEAT")
 }
+
+function parseNotes(take)
+	notes = {}
+	od_phrases={}
+	od=false
+	cur_od_phrase=1
+	_, notecount = reaper.MIDI_CountEvts(take)
+	
+	for i = 0, notecount - 1 do
+		_, _, _, spos, epos, _, pitch, _ = reaper.MIDI_GetNote(take, i)
+		ntime = reaper.MIDI_GetProjQNFromPPQPos(take, spos)
+		nend = reaper.MIDI_GetProjQNFromPPQPos(take, epos)
+		
+		if pitch == oP then
+			table.insert(od_phrases, {ntime,nend})
+		elseif pitch >= pR[diff][1][1] and pitch <= pR[diff][1][2] then
+			lane = pitch - pR[diff][1][1]
+			noteIndex = getNoteIndex(ntime, lane)
+			if noteIndex ~= -1 then
+				notes[noteIndex][2] = nend-ntime
+			else
+				table.insert(notes, { ntime, nend - ntime, lane, false, false })
+			end
+		elseif pitch >= pR[diff][2][1] and pitch <= pR[diff][2][2] then
+			lane = pitch - pR[diff][2][1]
+			noteIndex = getNoteIndex(ntime, lane)
+			
+			if noteIndex ~= -1 then
+				notes[noteIndex][4] = true
+			else
+				table.insert(notes, { ntime, -1, lane, true, false })
+			end
+		end
+	end
+	if #od_phrases~=0 then
+		for i=1,#notes do
+			if notes[i][1]>od_phrases[cur_od_phrase][2] then
+				if cur_od_phrase<#od_phrases then cur_od_phrase=cur_od_phrase+1 end
+			end
+			if notes[i][1]>=od_phrases[cur_od_phrase][1] and notes[i][1]<=od_phrases[cur_od_phrase][2] then
+				notes[i][5]=true
+			end
+		end
+	end
+end
 
 function updateMidi()
 	instrumentTracks={
@@ -133,7 +138,7 @@ function updateMidi()
 			local item = reaper.GetTrackMediaItem(instrumentTracks[inst][2], i)
 			local take = reaper.GetActiveTake(item)
 			local _,hash=reaper.MIDI_GetHash(take,true)
-			if midihash~=hash then
+			if midiHash~=hash then
 				parseNotes(take)
 				curNote=1
 				for i=1,#notes do
@@ -143,12 +148,44 @@ function updateMidi()
 					end
 					
 				end
-				midihash=hash
+				midiHash=hash
 			end
 		end
 	else
-		midihash=""
+		midiHash=""
 		notes={}
+	end
+end
+
+function updatebeatLines()
+	eventTracks={
+		findTrack("EVENTS"),
+		findTrack("BEAT")
+	}
+	if eventTracks[2] then
+		local numItems = reaper.CountTrackMediaItems(eventTracks[2])
+		for i = 0, numItems-1 do
+			local item = reaper.GetTrackMediaItem(eventTracks[2], i)
+			local take = reaper.GetActiveTake(item)
+			local _,hash=reaper.MIDI_GetHash(take,true)
+			if beatHash~=hash then
+				beatLines={}
+				_, notecount = reaper.MIDI_CountEvts(take)
+				for i = 0, notecount - 1 do
+					_, _, _, spos, _, _, pitch, _ = reaper.MIDI_GetNote(take, i)
+					btime = reaper.MIDI_GetProjQNFromPPQPos(take, spos)
+					db=true
+					if pitch==13 then
+						db=false
+					end
+					table.insert(beatLines,{btime,db})
+				end
+				beatHash=hash
+			end
+		end
+	else
+		beatHash=""
+		beatLines={}
 	end
 end
 
@@ -172,24 +209,24 @@ function drawNotes()
 			rend=rtime
 		end
 		
-		if rend<=0 and curnote~=#notes and curend<=0 then
+		if rend<=0 and curNote~=#notes and curend<=0 then
 			curNote=i+1
 		end
 		if rend>4 then
 			rend=4
 		end
 		
-		notescale=imgscale*(1-(nsm*rtime))
-		notescaleend=imgscale*(1-(nsm*rend))
+		noteScale=imgScale*(1-(nsm*rtime))
+		noteScaleEnd=imgScale*(1-(nsm*rend))
 		if diff<4 then
 			lane=lane+0.5
 		end
-		notex=((gfx.w/2)-(64*notescale)+((nxs*(1-(nxm*rtime)))*notescale*(lane-2)))
-		notey=gfx.h-(32*notescale)-(248*notescale)-((nyoff*rtime)*notescale)
-		susx=((gfx.w/2)+((nxs*(1-(nxm*rtime)))*notescale*(lane-2)))
-		susy=gfx.h-(248*notescale)-((nyoff*rtime)*notescale)
-		endx=((gfx.w/2)+((nxs*(1-(nxm*rend)))*notescaleend*(lane-2)))
-		endy=gfx.h-(248*notescaleend)-((nyoff*rend)*notescaleend)
+		notex=((gfx.w/2)-(64*noteScale)+((nxoff*(1-(nxm*rtime)))*noteScale*(lane-2)))
+		notey=gfx.h-(32*noteScale)-(248*noteScale)-((nyoff*rtime)*noteScale)
+		susx=((gfx.w/2)+((nxoff*(1-(nxm*rtime)))*noteScale*(lane-2)))
+		susy=gfx.h-(248*noteScale)-((nyoff*rtime)*noteScale)
+		endx=((gfx.w/2)+((nxoff*(1-(nxm*rend)))*noteScaleEnd*(lane-2)))
+		endy=gfx.h-(248*noteScaleEnd)-((nyoff*rend)*noteScaleEnd)
 		gfxid=2
 		if lift then gfxid=4 end
 		if od then 
@@ -205,34 +242,62 @@ function drawNotes()
 		end
 		if invalidLift then gfxid=6 end
 		gfx.r, gfx.g, gfx.b=1,1,1
-		gfx.blit(gfxid,notescale,0,0,0,128,64,notex,notey)
+		gfx.blit(gfxid,noteScale,0,0,0,128,64,notex,notey)
 		
 	end
 end
-lastCursorTime=reaper.TimeMap2_timeToQN(reaper.EnumProjects(-1),reaper.GetCursorPosition())
+
+function drawBeats()
+	width=300
+	if diff==4 then
+		width=425
+	end 
+	for i=curBeatLine,#beatLines do
+		btime=beatLines[i][1]
+		if btime>curBeat+(4/trackSpeed) then break end
+		if curBeat>beatLines[i][1]+2 then
+			curBeatLine=i
+		end
+		rtime=((btime-curBeat)*trackSpeed)+offset
+		beatScale=imgScale*(1-(nsm*rtime))
+		
+		sx=((gfx.w/2)-((width*(1-(nxm*rtime)))*beatScale))
+		ex=((gfx.w/2)+((width*(1-(nxm*rtime)))*beatScale))
+		y=gfx.h-(248*beatScale)-((nyoff*rtime)*beatScale)
+		gfx.line(sx,y,ex,y)
+		if beatLines[i][2] then
+			gfx.line(sx+1,y-1,ex-1,y-1)
+			gfx.line(sx-1,y+1,ex+1,y+1)
+		end
+	end
+end
+
 updateMidi()
+updatebeatLines()
+
 local function Main()
 	
-	imgscale=math.min(gfx.w,gfx.h)/1024
+	imgScale=math.min(gfx.w,gfx.h)/1024
 	local char = gfx.getchar()
 	if char ~= -1 then
 		reaper.defer(Main)
 	end
+
 	if char == 59 then -- [
 		if diff==1 then diff=4 else diff=diff-1 end
-		midihash=""
+		midiHash=""
 		updateMidi()
 	elseif char == 39 then -- ]
 		if diff==4 then diff=1 else diff=diff+1 end
-		midihash=""
+		midiHash=""
 		updateMidi()
 	elseif char == 91 then -- ;
 		if inst==1 then inst=4 else inst=inst-1 end
-		midihash=""
+		midiHash=""
 		updateMidi()
 	elseif char == 93 then -- '
 		if inst==4 then inst=1 else inst=inst+1 end
-		midihash=""
+		midiHash=""
 		updateMidi()
 	elseif char == 43 or char == 61 then -- +
 		trackSpeed = trackSpeed+0.05
@@ -247,12 +312,13 @@ local function Main()
 	-- if char~=0 then
 	-- 	reaper.ShowConsoleMsg(tostring(char).."\n")
 	-- end
+
 	gfx.setfont(1, "Arial", 16)
 	
 	if diff==4 then
-		gfx.blit(1,imgscale,0,0,0,1024,1024,(gfx.w/2)-(imgscale*512),gfx.h-(1024*imgscale)); 
+		gfx.blit(1,imgScale,0,0,0,1024,1024,(gfx.w/2)-(imgScale*512),gfx.h-(1024*imgScale)); 
 	else
-		gfx.blit(0,imgscale,0,0,0,1024,1024,(gfx.w/2)-(imgscale*512),gfx.h-(1024*imgscale));   
+		gfx.blit(0,imgScale,0,0,0,1024,1024,(gfx.w/2)-(imgScale*512),gfx.h-(1024*imgScale));   
 	end 
 	playState=reaper.GetPlayState()
 	if playState==1 then
@@ -268,6 +334,13 @@ local function Main()
 				break
 			end
 		end
+		curBeatLine=1
+		for i=1,#beatLines do
+			curBeatLine=i
+			if beatLines[i][1]>=curBeat-2 then
+				break
+			end
+		end
 	end
 	if curCursorTime~=lastCursorTime then
 		lastCursorTime=curCursorTime
@@ -275,6 +348,13 @@ local function Main()
 		for i=1,#notes do
 			curNote=i
 			if notes[i][1]+notes[i][2]>=curBeat then
+				break
+			end
+		end
+		curBeatLine=1
+		for i=1,#beatLines do
+			curBeatLine=i
+			if beatLines[i][1]>=curBeat-2 then
 				break
 			end
 		end
@@ -297,8 +377,12 @@ local function Main()
 		curBeat,
 		curNote
 	))
+	gfx.x,gfx.y=0,gfx.h-16
+	gfx.drawstr(string.format("Version %s",version_num))
 	updateMidi()
+	updatebeatLines()
 	drawNotes()
+	drawBeats()
 	gfx.update()
 end
 
